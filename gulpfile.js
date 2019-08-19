c338db5c3a6dbed9096e706bad235b9ce0845410
Lou Huang <lou@louhuang.com>
2019-08-19 15:51:54 -0400

    Revert "use CLI/Make build system"
    This reverts commit fb0f98cc0e4fde34f43d927ea121aceb7d37498e.

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var fs = require('fs')
+var gulp = require('gulp')
+var svgSprite = require('gulp-svg-sprite')
+
+var svgConfig = {
+  log: 'verbose',
+  shape: {
+    id: {
+      generator: 'image-%s'
+    }
+  },
+  mode: {
+    symbol: {
+      dest: '.',
+      sprite: 'images',
+      inline: true
+    }
+  }
+}
+
+var glob = [
+  'images/**/*.svg',
+  // Ignore
+  '!images/vehicles/fat-car-*.svg'
+]
+
+gulp.task('default', function () {
+  return gulp.src(glob)
+    .pipe(svgSprite(svgConfig))
+    .pipe(gulp.dest('./dist'))
+})
